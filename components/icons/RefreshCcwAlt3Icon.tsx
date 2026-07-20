36dd4c1e11b0281a2c2d6f8a6ffbe68101e7be47
Joel Hoke <joelhoke@gmail.com>
2026-07-20 14:17:14 -0700

    Improve playground sharing reliability

---
+++ components/icons/RefreshCcwAlt3Icon.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+/**
+ * Refresh CCW Alt 3 icon
+ * Source: SVG Repo, https://www.svgrepo.com/svg/533698/refresh-ccw-alt-3 (CC0)
+ */
+export default function RefreshCcwAlt3Icon({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      width="12"
+      height="12"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <path
+        d="M19.9381 13C19.979 12.6724 20 12.3387 20 12C20 7.58172 16.4183 4 12 4C9.49942 4 7.26681 5.14727 5.7998 6.94416M4.06189 11C4.02104 11.3276 4 11.6613 4 12C4 16.4183 7.58172 20 12 20C14.3894 20 16.5341 18.9525 18 17.2916M15 17H18V17.2916M5.7998 4V6.94416M5.7998 6.94416V6.99993L8.7998 7M18 20V17.2916"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
